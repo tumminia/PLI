bef980f6f740316821a1ec7fc247b802c5caaeaa
= <tumminia.p@outlook.it>
2025-08-06 22:25:59 +0200

    Update

--- LighthillWhithamRichards.py
+++ LighthillWhithamRichards.py
@@ -30,24 +30,24 @@ class LighthillWhithamRichards:
         for i in range(len(densita)):
             print(f"Tempo: {i}\nDensità: {densita[i]}\nFlusso: {flusso[i]}\n")
 
-    def chart(self, d_osservata, f_osservato, d_simulata, f_simulato):
+    def chart(self, d_1, f_1, d_2, f_2, titolo, tit_1, tit_2, lab_x1, lab_x2, lab_y):
 
         plt.figure(figsize=(12, 6))
-        plt.suptitle("Contronto tra flusso osservato e modello LWR")
+        plt.suptitle(titolo)
 
         plt.subplot(1, 2, 1)
-        plt.plot(d_osservata, f_osservato, alpha=0.7, color='teal', label=f'Flusso osservato')
-        plt.title("Flusso osservato")
-        plt.xlabel("Densità (veicoli/km)")
-        plt.ylabel("Flusso (veicoli/km)")
+        plt.plot(d_1, f_1, alpha=0.7, color='teal', label=lab_x1)
+        plt.title(tit_1)
+        plt.xlabel(lab_x1)
+        plt.ylabel(lab_y)
         plt.legend()
         plt.grid(True)
 
         plt.subplot(1, 2, 2)
-        plt.plot(d_simulata, f_simulato, alpha=0.7, color="red", label="Flusso simulato (LWR)")
-        plt.title("Flusso simulato (LWR)")
-        plt.xlabel("Densità (veicoli/km)")
-        plt.ylabel("Flusso (veicoli/km)")
+        plt.plot(d_2, f_2, alpha=0.7, color="red", label=lab_x2)
+        plt.title(tit_2)
+        plt.xlabel(lab_x2)
+        plt.ylabel(lab_y)
         plt.legend()
         plt.grid(True)
 
@@ -57,11 +57,11 @@ class LighthillWhithamRichards:
     def analizzaTraffico(self):
         data = self.leggiDati()
         num_celle = len(data)
-        x = 690.0
+        x = 8000.0
         dx = x/(num_celle - 1)
         dt = 0.01
         rho_max = 150.0
-        v_max = 70 * (1/3.6)
+        v_max = 120 * (1/3.6)
         densita = data['densita'].to_numpy()
 
         if v_max  * (dt/dx)>1:
@@ -73,7 +73,33 @@ class LighthillWhithamRichards:
             densita = self.conservazione(num_celle, densita, flusso, dx, dt)
         
         self.stampaStatoTraffico(densita, flusso)
-        self.chart(data['densita'].to_list(), data["flusso"].to_list(), densita, flusso)
+        self.chart( data['densita'].to_list(), data["flusso"].to_list(),
+             densita, flusso,
+             "Contronto tra flusso osservato e modello LWR",
+             "Flusso osservato",
+             "Flusso simulato (LWR)",
+             "Densità (veicoli/km)",
+             "Densità (veicoli/km)",
+             "Flusso (veicoli/km)"
+            )
+        self.chart( data['t'].to_list(), data['densita'].to_list(),
+             data['t'].to_list(), data["flusso"].to_list(),
+             "Modello Lighthill Whitham Richards",
+             "Densità",
+             "Flusso",
+             "Densità (veicoli/km)",
+             "Flusso (veicoli/km)",
+             "Tempo in periodo da 0 a t (s)"
+            )
+        self.chart( data['t'].to_list(), densita,
+             data['t'].to_list(), flusso,
+             "Modello Lighthill Whitham Richards",
+             "Densità",
+             "Flusso",
+             "Densità (veicoli/km)",
+             "Flusso (veicoli/km)",
+             "Tempo in periodo da 0 a t (s)"
+            )
 
 lwr = LighthillWhithamRichards()
 lwr.analizzaTraffico()
