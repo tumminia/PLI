999b24aebd015d15ab593c0932e069c97bd5ed37
= <tumminia.p@outlook.it>
2025-08-11 19:47:49 +0200

    Update

--- LighthillWhithamRichards.py
+++ LighthillWhithamRichards.py
@@ -104,6 +104,6 @@ class LighthillWhithamRichards:
         )
 
 lwr = LighthillWhithamRichards()
-lwr.analizzaTraffico("CSV/E17_flusso_alto.csv", 25)
-lwr.analizzaTraffico("CSV/E17_flusso_medio.csv", 85)
+lwr.analizzaTraffico("CSV/E17_flusso_alto.csv", 30)
+lwr.analizzaTraffico("CSV/E17_flusso_medio.csv", 60)
 lwr.analizzaTraffico("CSV/E17_flusso_basso.csv", 120)
